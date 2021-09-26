ad5a9a8a95bc7009a79f267538cb39528854ac62
Wendelin Gerein <info@webdelin.de>
2021-09-26 20:41:39 +0200

    New Deployment

---
+++ js/chunk-2d0c1282.a513cc0c.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0c1282"],{"459d":function(e,n,c){"use strict";c.r(n);var r=c("7a23");function t(e,n,c,t,o,u){var w=Object(r["E"])("router-view");return Object(r["w"])(),Object(r["e"])(w)}var o={},u=c("6b0d"),w=c.n(u);const a=w()(o,[["render",t]]);n["default"]=a}}]);
+//# sourceMappingURL=chunk-2d0c1282.a513cc0c.js.map
---
+++ precache-manifest.f313563083742251b2fc4e26f2ca2729.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c16ac83ee5fca052daab2f17d22577e0",
+    "url": "/vue3-shop-demo/categories.json"
+  },
+  {
+    "revision": "ad6390aa6a889a648037",
+    "url": "/vue3-shop-demo/css/app.cf8cc90e.css"
+  },
+  {
+    "revision": "d2de48652d20f53863fb",
+    "url": "/vue3-shop-demo/css/chunk-29805a02.72f5cfe5.css"
+  },
+  {
+    "revision": "7333c75709a0bf832036",
+    "url": "/vue3-shop-demo/css/chunk-7e1253b0.45e4b825.css"
+  },
+  {
+    "revision": "071441214765ae1bd26e54083ffefed6",
+    "url": "/vue3-shop-demo/index.html"
+  },
+  {
+    "revision": "ad6390aa6a889a648037",
+    "url": "/vue3-shop-demo/js/app.49f69861.js"
+  },
+  {
+    "revision": "f243818168ed4036f9d6",
+    "url": "/vue3-shop-demo/js/chunk-001c8fbc.263329c1.js"
+  },
+  {
+    "revision": "906bb20ba9f3adaad4f0",
+    "url": "/vue3-shop-demo/js/chunk-257bbe34.475211bd.js"
+  },
+  {
+    "revision": "d2de48652d20f53863fb",
+    "url": "/vue3-shop-demo/js/chunk-29805a02.40fc3dd6.js"
+  },
+  {
+    "revision": "402c6c66dd828bb35b7e",
+    "url": "/vue3-shop-demo/js/chunk-2d0c1282.a513cc0c.js"
+  },
+  {
+    "revision": "0d5be3e58aa73af1c701",
+    "url": "/vue3-shop-demo/js/chunk-2d0c518a.bebd8e54.js"
+  },
+  {
+    "revision": "f4c5e4291aeb8a664e1d",
+    "url": "/vue3-shop-demo/js/chunk-30e8c609.702263ae.js"
+  },
+  {
+    "revision": "5b81710dcff1133d118f",
+    "url": "/vue3-shop-demo/js/chunk-47e5d611.28c41f66.js"
+  },
+  {
+    "revision": "b90387bfcc2c3c2e5658",
+    "url": "/vue3-shop-demo/js/chunk-708a681d.c7f4965a.js"
+  },
+  {
+    "revision": "5e62dc15a4024bc8aef9",
+    "url": "/vue3-shop-demo/js/chunk-723d1dec.f51b3f74.js"
+  },
+  {
+    "revision": "7333c75709a0bf832036",
+    "url": "/vue3-shop-demo/js/chunk-7e1253b0.854466cd.js"
+  },
+  {
+    "revision": "6a5678d31fbc9ac139a6",
+    "url": "/vue3-shop-demo/js/chunk-7fb516ea.bacf140a.js"
+  },
+  {
+    "revision": "8f0f5ec6bd0f659b1a7e",
+    "url": "/vue3-shop-demo/js/chunk-vendors.abc0cf51.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/vue3-shop-demo/manifest.json"
+  },
+  {
+    "revision": "c952ea879a9c8002006232acf59fd961",
+    "url": "/vue3-shop-demo/products.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue3-shop-demo/robots.txt"
+  }
+]);
